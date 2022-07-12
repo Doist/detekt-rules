842f708ef088c4c8276f926e8a445cfb791c1f65
GitHub Action <action@github.com>
2022-07-12 13:53:09 +0000

    Change package name from org.example.detekt to com.github.doist.detektrules

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
     `maven-publish`
 }
 
-group = "org.example.detekt"
+group = "com.github.doist.detektrules"
 version = "1.0-SNAPSHOT"
 
 repositories {
--- src/test/kotlin/com/github/doist/detektrules/MyRuleTest.kt
+++ src/test/kotlin/com/github/doist/detektrules/MyRuleTest.kt
@@ -1,4 +1,4 @@
-package org.example.detekt
+package com.github.doist.detektrules
 
 import io.gitlab.arturbosch.detekt.api.Config
 import io.gitlab.arturbosch.detekt.rules.KotlinCoreEnvironmentTest
